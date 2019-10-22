017190419410d1959cd93c4e6c6b6700fb8ba339
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-22 13:41:49 +0100

    update html to include svg element

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-d3.select("#reset")
-    .on("click", function() {
-      d3.selectAll(".letter")
-        .remove();
+d3.select('#reset')
+  .on('click', () => {
+    d3.selectAll('.letter')
+      .remove();
 
-      d3.select("#phrase")
-          .text("");
+    d3.select('#phrase')
+      .text('');
 
-      d3.select("#count")
-          .text("");
-    });
+    d3.select('#count')
+      .text('');
+  });
 
-d3.select("form")
-    .on("submit", function() {
-      d3.event.preventDefault();
-      var input = d3.select("input");
-      var text = input.property("value");
+d3.select('form')
+  .on('submit', function() {
+    d3.event.preventDefault();
+    var input = d3.select('input');
+    var text = input.property('value');
 
-      var letters = d3.select("#letters")
-                      .selectAll(".letter")
-                      .data(getFrequencies(text), function(d) {
-                        return d.character;
-                      });
+    var letters = d3.select('#letters')
+      .selectAll('.letter')
+      .data(getFrequencies(text), function(d) {
+        return d.character;
+      });
 
-      letters
-          .classed("new", false)
-        .exit()
-        .remove();
+    letters
+      .classed('new', false)
+      .exit()
+      .remove();
 
-      letters                
-        .enter()
-        .append("div")
-          .classed("letter", true)
-          .classed("new", true)
-        .merge(letters)
-          .style("width", "20px")
-          .style("line-height", "20px")
-          .style("margin-right", "5px")
-          .style("height", function(d) {
-            return d.count * 20 + "px";
-          })
-          .text(function(d) {
-            return d.character;
-          });
+    letters
+      .enter()
+      .append('div')
+      .classed('letter', true)
+      .classed('new', true)
+      .merge(letters)
+      .style('width', '20px')
+      .style('line-height', '20px')
+      .style('margin-right', '5px')
+      .style('height', function(d) {
+        return d.count * 20 + 'px';
+      })
+      .text(function(d) {
+        return d.character;
+      });
 
-      d3.select("#phrase")
-          .text("Analysis of: " + text);
+    d3.select('#phrase')
+      .text('Analysis of: ' + text);
 
-      d3.select("#count")
-          .text("(New characters: " + letters.enter().nodes().length + ")");
+    d3.select('#count')
+      .text('(New characters: ' + letters.enter().nodes().length + ')');
 
-      input.property("value", "");
-    });
+    input.property('value', '');
+  });
 
 function getFrequencies(str) {
-  var sorted = str.split("").sort();
+  var sorted = str.split('').sort();
   var data = [];
   for (var i = 0; i < sorted.length; i++) {
     var last = data[data.length - 1];
